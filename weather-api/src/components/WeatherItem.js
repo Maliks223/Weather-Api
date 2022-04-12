d45c6b575f62711ace91a0989875680aa53faad3
maram <mmaramaali22@gmail.com>
2022-04-12 11:46:07 +0300

    without margin

--- weather-api/src/components/WeatherItem.js
+++ weather-api/src/components/WeatherItem.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const WeatherItem = (props) => {
   return(
+    
     <div className="card">
           <p>{props.hour}</p>
             <img src={props.image} alt="sun icon" className="card-icon"></img>
