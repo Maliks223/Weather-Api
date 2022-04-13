a0d3254d16a3c3c80b361ff8917cca7c6134f5a4
maram <mmaramaali22@gmail.com>
2022-04-13 15:33:53 +0300

    Final Edit

--- weather-api/src/App.js
+++ weather-api/src/App.js
@@ -3,69 +3,37 @@ import "./App.css";
 
 import Search from "./components/Search";
 import LiveWeather from "./components/LiveWeather";
-import WeatherTime from "./components/WeatherTime";
-
-import fakeWeatherData from "./fakeWeatherData.json";
-
-import clear from "./img/weather-icons/clear.svg";
-import storm from "./img/weather-icons/storm.svg";
-import drizzle from "./img/weather-icons/drizzle.svg";
-import rain from "./img/weather-icons/rain.svg";
-import snow from "./img/weather-icons/snow.svg";
-import mostlycloudy from "./img/weather-icons/mostlycloudy.svg";
-import partlycloudy from "./img/weather-icons/partlycloudy.svg";
-import fog from "./img/weather-icons/fog.svg";
+import WeatherTime, { findSrc } from "./components/WeatherTime";
 
 
 const App = () => {
 
-//  const nowData = fakeWeatherData.list[0];
+
   const hoursData = [];
-  // for(let i=1; i<=7; i++){
-  //   hoursData.push(fakeWeatherData.list[i]);
-  // }
-  const id = fakeWeatherData.list[0].weather[0].id;
+
   
-  const findSrc =(id) => {
-  if (id < 300) {
-    return storm
-  }
-  else if (300 < id && id < 499) {
-    return drizzle
-  }
-  else if (500 < id && id < 599) {
-    return rain
-  }
-  else if (600 < id && id < 699) {
-    return snow
-  }
-  else if (700 < id && id < 799) {
-    return fog
-  } else if (id == 800) {
-    return clear
-  }
-  else if (id == 801) {
-    return partlycloudy
-  }
-  else { return mostlycloudy }
-  }
 
   const callApi = async (city) => {
-    if(city === "") return;
+    if(city === "") {
+      alert("Enter a city name")
+        return
+    };
     const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=8&units=metric&appid=402eb8aeb1f6d523ecb76859e056a1ef`)
-    const result = await response.json();
-    // console.log("result : ");
-    // console.log(result);
-    if(result.cod === '404') {
-      alert(result.message);
-    } else {
-      setList(result.list)
-      // console.log("List: ");
-      // console.log(list);
-    }
+    .then((res)=> res.json()
+
+    ).then(result =>{
+      console.log(result);
+      if(result.cod === '404') {
+        alert(result.message);
+      } else {
+        setList(result.list)
+      }
+    }).catch((err)=>{
+      alert("No Internet Connection")
+    })
   }
 
-  // const [inputVal, setInputVal] = useState('beirut');
+ 
   const [list, setList] = useState(null);
 
     return (
@@ -78,9 +46,9 @@ const App = () => {
           maxTemp={list[0].main.temp_max}
           humidity={list[0].main.humidity}
           pressure={list[0].main.pressure}
-          iconSrc = {findSrc(fakeWeatherData.list[0].weather[0].id)}
+          iconSrc = {findSrc(list[0].weather[0].id, true)}
           />}
-          {list && list.map((item, index) => index !== 0 && hoursData.push(item)) && <WeatherTime Hdata={hoursData}/>}
+          {list && list.map((item, index) => index !== 0 && hoursData.push(item)) && <WeatherTime Hdata={hoursData} />}
         </main>
       </div>
       
--- weather-api/src/components/WeatherTime.js
+++ weather-api/src/components/WeatherTime.js
@@ -12,46 +12,69 @@ import mostlycloudy from "../img/weather-icons/mostlycloudy.svg";
 import partlycloudy from "../img/weather-icons/partlycloudy.svg";
 import fog from "../img/weather-icons/fog.svg";
 
-const findSrc =(id) => {
+export const findSrc = (id, flag) => {
     if (id < 300) {
-      return storm
+        return storm
     }
-    else if (300 < id && id < 499) {
-      return drizzle
+    else if (300 <= id && id <= 499) {
+        return drizzle
     }
-    else if (500 < id && id < 599) {
-      return rain
+    else if (500 <= id && id <= 599) {
+        if (flag) {
+            document.body.style.backgroundColor = "#a3b5c1"
+            document.querySelector("#header").style.backgroundColor = "#7287ac"
+            document.querySelector(".button").style.backgroundColor = "#5366a5"
+        }
+
+        return rain
     }
-    else if (600 < id && id < 699) {
-      return snow
+    else if (600 <= id && id <= 699) {
+        return snow
     }
-    else if (700 < id && id < 799) {
-      return fog
-    } else if (id == 800) {
-      return clear
+    else if (700 <= id && id <= 799) {
+        return fog
     }
-    else if (id == 801) {
-      return partlycloudy
+    else if (id === 800) {
+        if (flag) {
+            document.body.style.backgroundColor = "#2490ee"
+            document.querySelector("#header").style.backgroundColor = "#1b6cd4"
+            document.querySelector(".button").style.backgroundColor = "#1851b9"
+        }
+        return clear
     }
-    else { return mostlycloudy }
+    else if (id === 801) {
+        if (flag) {
+            document.body.style.backgroundColor = "#9ccef4"
+            document.querySelector("#header").style.backgroundColor = "#759eda"
+            document.querySelector(".button").style.backgroundColor = "#5879c7"
+        }
+        return partlycloudy
     }
-    
+    else if (801 <= id && id <= 805) {
+        if (flag) {
+            document.body.style.backgroundColor = "#9ccef4"
+            document.querySelector("#header").style.backgroundColor = "#759eda"
+            document.querySelector(".button").style.backgroundColor = "#5879c7"
+        }
+        return mostlycloudy
+    }
+}
+
 
 
 const WeatherTime = (props) => {
 
-    const hoursArray = props.Hdata.map((section)=>{
-        return <WeatherItem key={section.dt} hour={section.dt_txt.split(' ')[1].slice(0,5)}
-        image={findSrc(section.weather[0].id)} temperature={Math.floor(section.main.temp)}/>
+    const hoursArray = props.Hdata.map((section) => {
+        return <WeatherItem key={section.dt} hour={section.dt_txt.split(' ')[1].slice(0, 5)}
+            image={findSrc(section.weather[0].id, false)} temperature={Math.floor(section.main.temp)} />
     })
-    
-        
-    
-    return(
+
+
+    return (
         <div className="cards">
             {hoursArray}
         </div>
-        )
-  }
-  
-  export default WeatherTime;
+    )
+}
+
+export default WeatherTime;
